483e3e34dfe4542d5b3de9eed06868f814ad1a0e
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-08-22 23:20:36 +0300

    add

---
+++ __test__/events.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Events = require('events')
+const events = new Events();
+
+describe('module Node', () => {
+    it('events', () => {
+        expect(events).toBeInstanceOf(Events)
+    })
+})
